26247346f3a586172805c8db5be6ba8b8b5eb17e
anette87 <anettem.rivera@gmail.com>
2020-10-07 17:00:42 -0700

    Procfile 3

---
+++ build/precache-manifest.a1b2ada58996825a6c7e37745ca21c62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98040b250fb3f224e6c38808afc7c506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cd1d34d33f13de3c9ba",
+    "url": "/static/css/main.ba01f578.chunk.css"
+  },
+  {
+    "revision": "dc188885c76d2637b7cb",
+    "url": "/static/js/2.ef735ab6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef735ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd1d34d33f13de3c9ba",
+    "url": "/static/js/main.20e5e7a7.chunk.js"
+  },
+  {
+    "revision": "de15eed02133cb36b36f",
+    "url": "/static/js/runtime-main.d7276c7c.js"
+  },
+  {
+    "revision": "8883e8d0b2dbb854e0a0fbf03f7b741c",
+    "url": "/static/media/closet.8883e8d0.jpg"
+  }
+]);
