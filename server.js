0c048ebc6a05a244dad6d686a88bbc2a518aa62f
anette87 <anettem.rivera@gmail.com>
2020-10-07 17:33:42 -0700

    Update server.js for heroku launch

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+let app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
+
 // const path = require('path');
 // const express = require('express');
 // const app = express();
@@ -11,11 +21,6 @@
 //     console.log(path.join(publicPath, '/index.html'));
 //    res.sendFile(path.join(publicPath, '/index.html'));
 // });
-
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
 
 // app.listen(port, () => {
 //    console.log('Server is up!');
